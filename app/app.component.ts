c3da97d877ae0d4065da83fb59a4733aeeac08c1
tarlepp <tarmo.leppanen@protacon.com>
2017-03-30 18:02:52 +0300

    Improved demos

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,4 +7,43 @@ import { Component, ViewEncapsulation } from '@angular/core'
   encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent { }
+export class AppComponent {
+  public layouts = [
+    {
+      name: 'Alpha-Numeric',
+      layout: 'alphaNumeric',
+      placeholder: 'placeholder 1',
+      placeholderCustom: 'custom placeholder 1',
+    },
+    {
+      name: 'Alpha-Numeric - Nordic',
+      layout: 'alphaNumericNordic',
+      placeholder: 'placeholder 2',
+      placeholderCustom: 'custom placeholder 2',
+    },
+    {
+      name: 'Extended',
+      layout: 'extended',
+      placeholder: 'placeholder 3',
+      placeholderCustom: 'custom placeholder 3',
+    },
+    {
+      name: 'Extended - Nordic',
+      layout: 'extendedNordic',
+      placeholder: 'placeholder 4',
+      placeholderCustom: 'custom placeholder 4',
+    },
+    {
+      name: 'Numeric',
+      layout: 'numeric',
+      placeholder: 'placeholder 5',
+      placeholderCustom: 'custom placeholder 5',
+    },
+    {
+      name: 'Phone',
+      layout: 'phone',
+      placeholder: 'placeholder 6',
+      placeholderCustom: 'custom placeholder 6',
+    },
+  ]
+}
