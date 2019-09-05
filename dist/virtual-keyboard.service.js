18b55272944cda234767e050e2137cd0fff09497
Konstantin Zhulidov <kzhulidov@kns.com>
2019-09-05 17:06:14 +0200

    Update build artifacts

--- dist/virtual-keyboard.service.js
+++ dist/virtual-keyboard.service.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var ReplaySubject_1 = require("rxjs/internal/ReplaySubject");
@@ -58,9 +64,9 @@ var VirtualKeyboardService = /** @class */ (function () {
     VirtualKeyboardService.prototype.reset = function () {
         this.setShift(false);
     };
-    VirtualKeyboardService.decorators = [
-        { type: core_1.Injectable },
-    ];
+    VirtualKeyboardService = __decorate([
+        core_1.Injectable()
+    ], VirtualKeyboardService);
     return VirtualKeyboardService;
 }());
 exports.VirtualKeyboardService = VirtualKeyboardService;
