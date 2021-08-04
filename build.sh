d86688eada730db5c7b05509b037e33fd5e4d3c4
Heikki-Jussi Niemi <hekku2@gmail.com>
2021-08-04 14:11:50 +0300

    Add executionscript

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -w /app -v ${PWD}/:/app node:12-alpine3.14 yarn install ; yarn build
--- lint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run -it --rm -w /app -v ${PWD}/:/app node:12-alpine3.14 yarn lint
-
